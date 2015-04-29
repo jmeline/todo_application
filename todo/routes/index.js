f5e8f97abb49664638c013e927e8465ceb3ccf88
Jacob Meline <jacob.meline@gmail.com>
2015-04-29 14:02:48 -0600

    Adding buttons: list, add, delete, and edit

--- todo/routes/index.js
+++ todo/routes/index.js
@@ -4,14 +4,7 @@ var router = express.Router();
 // /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index',
-    {title: "Todo Application", body : "This is an example page"});
+    {title: "Todo App", body : "TODO Application"});
 });
 
-// module.exports = function(app)
-// {
-//     app.get("/", function(req, res)
-//     {
-//         res.render("index", {title: "Todo Application", body : "This is an example page"});
-//     });
-// }
 module.exports = router;

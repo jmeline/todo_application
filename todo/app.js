35d10ae7c87ff16560d3520aa2d6461d515c8bc9
Jacob Meline <jacob.meline@gmail.com>
2015-04-30 20:20:22 -0600

    More backend work got done. Still need to get tasks working in the back end

--- todo/app.js
+++ todo/app.js
@@ -1,69 +1,55 @@
 //
-//    My Application
+//    My TODO Application
 //
-var express       = require('express');
-var path          = require('path');
-var favicon       = require('serve-favicon');
-var logger        = require('morgan');
+var express          = require('express');
+var path                = require('path');
+var http                = require('http');
+var favicon           = require('serve-favicon');
+var logger            = require('morgan');
 var cookieParser  = require('cookie-parser');
 var bodyParser    = require('body-parser');
+var routes            = require('./routes/index');
+var users             = require('./routes/users');
+var tasks              = require('./routes/tasks');
 
-var routes        = require('./routes/index');
-var users         = require('./routes/users');
+var mongoskin     = require('mongoskin');
+var db = mongoskin.db('mongodb://localhost:27017/todo?auto_reconnect', {safe:true});
 
-var app           = express();
+var app = express();
+app.use(function(req, res, next){
+    req.db = {};
+    req.db.tasks = db.collection('tasks');
+    next();
+})
+
+app.set('port', process.env.PORT || 3000);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'jade');
 app.set('view engine', 'vash');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-
-// set port to the environment if provided, else use 3000
-app.set('port', process.env.PORT || 3000);
-
-
+// Set routes
 app.use('/', routes);
+app.get('/tasks', tasks.list);
+app.post('/tasks', tasks.markAllCompleted)
+app.post('/tasks', tasks.add);
+
 app.use('/users', users);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
-}
-
-// production error handler
-// no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
 });
 
+app.listen(app.get('port)'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
 
 module.exports = app;
